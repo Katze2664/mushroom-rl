Fixed incorrect broadcasting in TorchApproximator loss function
When using ActionRegressor, Network must squeeze action dimension to avoid incorrect broadcasting in TorchApproximator loss function.

examples/acrobot_dqn.py
@@ -37,7 +37,7 @@ class Network(nn.Module):
         q = self._h3(features2)
 
         if action is None:
-            return q
+            return q.squeeze(axis=1)
         else:
             action = action.long()
             q_acted = torch.squeeze(q.gather(1, action))
@@ -74,6 +74,12 @@ def experiment(n_epochs, n_steps, n_steps_test):
     n_features = 80
     train_frequency = 1
 
+    use_qregressor = False
+    if use_qregressor:
+        output_shape = mdp.info.action_space.size  # QRegressor
+    else:
+        output_shape = (1,)  # ActionRegressor
+    
     # Approximator
     input_shape = mdp.info.observation_space.shape
     approximator_params = dict(network=Network,
@@ -82,7 +88,7 @@ def experiment(n_epochs, n_steps, n_steps_test):
                                loss=F.smooth_l1_loss,
                                n_features=n_features,
                                input_shape=input_shape,
-                               output_shape=mdp.info.action_space.size,
+                               output_shape=output_shape,
                                n_actions=mdp.info.action_space.n)
 
     # Agent

mushroom_rl/approximators/_implementations/action_regressor.py
@@ -77,6 +77,11 @@ class ActionRegressor(Serializable):
             The predictions of the model.
 
         """
+        # Note: To avoid incorrect broadcasting in the loss function of TorchApproximator.fit(), when using
+        # ActionRegressor then TorchApproximator.network(state) must return a tensor whose action dimension has been
+        # squeezed. For this reason ActionRegressor.predict() must expect ActionRegressor.model[i].predict(state)
+        # to return an array whose action dimension has been squeezed.
+
         assert len(z) == 1 or len(z) == 2
 
         state = z[0]
@@ -90,8 +95,7 @@ class ActionRegressor(Serializable):
             for i in range(self._n_actions):
                 idxs = np.argwhere((action == i)[:, 0]).ravel()
                 if idxs.size:
-                    q[idxs] = self.model[i].predict(state[idxs],
-                                                    **predict_params).squeeze(axis=1)
+                    q[idxs] = self.model[i].predict(state[idxs], **predict_params)
         else:
             if len(self._output_shape) == 1:
                 q = np.zeros((state.shape[0], self._n_actions))
@@ -99,8 +103,7 @@ class ActionRegressor(Serializable):
                 q = np.zeros((state.shape[0], self._n_actions, self._output_shape[1]))
             
             for i in range(self._n_actions):
-                q[:, i] = self.model[i].predict(state,
-                                                **predict_params).squeeze(axis=1)
+                q[:, i] = self.model[i].predict(state, **predict_params)
 
         return q
 